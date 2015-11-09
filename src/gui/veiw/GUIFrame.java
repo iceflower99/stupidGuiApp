a328882d0813ff5e5875e1a987293bb6970845a9
iceflower99 <mhal4767@740s-pl-120.local>
2015-11-09 09:55:53 -0700

    added new stuff

--- src/gui/veiw/GUIFrame.java
+++ src/gui/veiw/GUIFrame.java
@@ -5,19 +5,28 @@ import gui.controller.GUIAppController;
 
 /**
  * @author madeleine hales
- *@ version 0.xNov 9, 2015
+ *@ version 0.2xNov 9, 2015 updated the setupFrame to load the contentOane with a GUIPanel.
  */
 public class GUIFrame extends JFrame
 {
 	private GUIAppController baseController;
-    public GUIFrame(GUIAppController baseController)
+    private GUIPanel basePanel;
+    
+	
+	public GUIFrame(GUIAppController baseController)
     {
     	this.baseController=baseController;
     }
     
     private void setUpFrame()
     {
-    	
+    	this.setContentPane(basePanel);
+    	this.setSize(400,400);
+    	this.setResizable(false);
+    	this.setVisible(true);
     }
-
+public GUIAppController getBaseontroller()
+{
+	return baseController;
+}
 }
---
+++ src/gui/veiw/GUIPanel.java
@@ -0,0 +1,33 @@
+package gui.veiw;
+
+import gui.controller.GUIAppController;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class GUIPanel extends JPanel
+{
+ private GUIAppController baseController;
+ private JButton firstButton;
+ private JTextField firstTextField;
+ private SpringLayout baseLayout;
+ public GUIPanel(GUIAppController baseController)
+ {
+	 this.baseController = baseController;
+	 
+	 baseLayout = new SpringLayout();
+	 firstButton = new JButton("Please do not click the button");
+	 firstTextField = new JTextField("words can be typed here");
+	 
+	 setupPanel();
+	 setupLayout();
+	 setupListeners();
+ }
+   private void setupPanel()
+   {
+	   this.setLayout(baseLayout);
+	   this.add(firstButton);
+	   this.add(firstTextField);
+   }
+}
